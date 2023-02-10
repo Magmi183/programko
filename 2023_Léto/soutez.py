ad8d5f1ab975833489336579239f79741429bf48
Michal Janecek <michal.janecek@gmail.com>
2023-02-11 00:00:11 +0100

    SOUTEZ: Lekce 1 kviz, pouze skupina 1.

---
+++ 2023_Léto/soutez.py
@@ -0,0 +1,21 @@
+"""
+PRAVIDLA:
+
+Pokud je na kroužku aspoň 7 lidí, hodnotí se prvních 5 míst:
+1. místo = 5 lístků do osudí
+2. místo = 4 lístky do osudí
+3. místo = 3 lístky do osudí
+4. místo = 2 lístky do osudí
+5. místo = 1 lístek do osudí
+
+Jinak se hodnotí první 3 místa:
+1. místo = 3 lístky do osudí
+2. místo = 2 lístky do osudí
+3. místo = 1 lístky do osudí
+"""
+
+osudi_1 = {"Petr": [3],
+           "Pavel": [2],
+           "Kuba": [1]}
+
+osudi_2 = {}
