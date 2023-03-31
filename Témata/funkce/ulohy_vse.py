6bfe216b2b3450f156ef75f73c31c192f27ef061
Michal Janecek <michal.janecek@gmail.com>
2023-03-31 14:16:01 +0200

    Přidáno něco málo k funkcím, ale TODO jsou stále TODO :D

---
+++ Témata/funkce/ulohy_vse.py
@@ -0,0 +1,94 @@
+# Úlohy na funkce, předpokládají znalost parametrů i návratové hodnoty
+# >> úlohy nemusí být seřazeny dle obtížnosti
+#  - - - - - - - - - - - - - - - - - - - - - - - > > > > > > > > > > > >
+
+"""
+Úloha 1: Je to palindrom?
+
+Udělej funkci, která uživateli řekne, zda-li je danný string palindrom. Palindrom je text, který se dá číst i pozpátku a má stejný význam.
+
+PARAMETRY FUNKCE:
+    - text (u kterého chceme ověřit, zda-li se jedná o palindrom)
+
+NÁVRATOVÉ HODNOTA:
+    - boolean (True nebo False), podle toho, jestli se jedná o palindrom (True), nebo ne (False)
+"""
+
+
+"""
+Úloha 1: Je to palindrom?
+
+Udělej funkci, která uživateli řekne, zda-li je danný string palindrom. Palindrom je text, který se dá číst i pozpátku a má stejný význam.
+
+PARAMETRY FUNKCE:
+    - text (u kterého chceme ověřit, zda-li se jedná o palindrom)
+
+NÁVRATOVÉ HODNOTA:
+    - boolean (True nebo False), podle toho, jestli se jedná o palindrom (True), nebo ne (False)
+"""
+
+# PŘEVODNÍK JEDNOTEK - ÚLOHA 2.
+# >>>> Následující 3 úlohy na sebe postupně navazují
+
+"""
+>   Úloha 2a: Převodník jednotek (teplota)
+    
+    Udělej funkci, která převede stupně Celsia na stupně Fahrenheita a naopak.
+    
+    PARAMETRY FUNKCE:
+        - číslo (stupně k převodu)
+        - mód (který bude udávat, jestli se převádí z C do F, nebo naopak)
+    
+    NÁVRATOVÁ HODNOTA:
+        - převedené stupně
+    
+"""
+
+"""
+>   Úloha 2b: Převodník jednotek (délka)
+    
+    Udělej funkci, která bude převádět mezi palci a centimetry.
+    
+    PARAMETRY FUNKCE:
+        - číslo (délka k převodu)
+        - mód (který bude udávat, jestli se z palců do cm, nebo naopak)
+    
+    NÁVRATOVÁ HODNOTA:
+        - převedená délka
+    
+"""
+
+"""
+>   Úloha 2c: Převodník jednotek (univerzální)
+
+    Udělej funkci, která bude umět převádět jak délku tak stupně, podle toho co si uživatel přeje.
+
+    PARAMETRY FUNKCE:
+        - číslo (hodnota k převodu)
+        - další parametry, dle kterých lze specifikovat, jestli chceme převádět délku či stupně, a také odkud kam
+
+    NÁVRATOVÁ HODNOTA:
+        - převedená hodnota (např. stupně z F do C), podle toho co si uživatel přál
+
+"""
+
+
+
+# TODO: Caesarova šifra, BMI kalkulátor, password strength checker ?
+
+"""
+    Úloha 3: Caesarova šifra
+
+    Tato úloha je rozdělena na 3 části, které na sebe postupně navazují.
+    
+    
+    Udělej funkci, která převede stupně Celsia na stupně Fahrenheita a naopak.
+
+    PARAMETRY FUNKCE:
+        - číslo (stupně k převodu)
+        - mód (který bude udávat, jestli se převádí z C do F, nebo naopak)
+
+    NÁVRATOVÁ HODNOTA:
+        - převedené stupně
+
+"""
