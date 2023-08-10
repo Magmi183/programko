9afd2e93c5910f82d585e9b9b28e32c8a2960097
Michal Janecek <michal.janecek@gmail.com>
2023-08-10 15:45:27 +0200

    Připravení tomboly.

---
+++ VÝUKA/tombola.py
@@ -0,0 +1,23 @@
+"""
+PRAVIDLA:
+
+Lístky do tomboly se získávájí za:
+  1) Pořadí ve kvízech
+    1. místo = 5 lístků do osudí
+    2. místo = 4 lístky do osudí
+    3. místo = 3 lístky do osudí
+    4. místo = 2 lístky do osudí
+    5. místo = 1 lístek do osudí
+
+  2) Řešení úloh (mini-soutěže) a programování větších programů
+    - Vyřešení malé úlohy (na rychlost): 1 lístek
+    - Naprogramování většího projektu: 1 - 10 lístků
+
+
+CENY PRO VÍTĚZE: Krypto
+
+"""
+
+osudi_1 = {}
+osudi_2 = {}
+osudi_3 = {}
