08c01c57bd372a2d40eac39e05a27690dee43275
Michal Janecek <michal.janecek@gmail.com>
2023-09-07 14:23:26 +0200

    odstraneni ulohy stuidum na VŠ, přidani pokut.

--- Témata/podminky/ulohy_podminky.py
+++ Témata/podminky/ulohy_podminky.py
@@ -47,22 +47,29 @@ Načtěte od uživatele tři čísla a řekněte, které z nich je největší. 
 """
 
 """
-Úkol 5: Studium na vysoké škole
+Úkol 5: Pokuta
 
-Udělejte program, který uživateli řekne, jestli může nastoupit na VŠ.
-Podmínky jsou následující: 
-- musí mu být 18 let a víc
-- musí mít maturitu
-- musí mít složené přijímačky
-- Pokud je jeho jméno Albert Einstein, nemusí splňovat nic z výše uvedeného.
-
-Použijte kód níže. Pro otestování programu měňte hodnoty z False na True a obráceně.
+Udělejte program, který na základě informací o rychlosti a lokaci řidiče vyhodnotí, jestli dostane pokutu, případně jak bude velká.
+Uživatel zadá tři informace:
+    - rychlost v kilometrech za hodinu (km/h)
+    - z jaké je země (Německo nebo ČR), jiné země program nezná
+    - jestli je to policajt (ano/ne)
+    
+PRAVIDLA:
+Pokud je uživatel policajt, nikdy žádnou pokutu nedostane, protože byl na služebním výjezdu.
+Pokud je uživatel z NĚMECKA, tak platí jediné pravidlo - pokud jede 0 km/h, tedy stojí, dostane pokutu 1 milion kč,
+v ostatních případech ale nedostane nic - v Německu na dálnici není rychlostní omezení.
+Pokud je ale z ČR, tak:
+    - pokud jede 0 km/h, dostane pokutu 1 milion kč za stání na silnici
+    - pokud jede mezi 1 - 130 km/h tak pokutu nedostane
+    - pokud jede více než 130 km/h, dostane pokudu 1000 kč
+    
+Na základě vstupu od uživatele rozhodněte a vypište výši pokuty.
 """
 
-vek = int(input("Kolik je ti let?\n"))
-celejmeno = input("Zadej své celé jméno.\n")
-maturita = True
-prijimacky = True
+rychlost = int(input("Zadej rychlost:\n"))
+zeme = input("Zadej zemi: (Německo nebo ČR):\n")
+policie = input("Jsi policie? Zadej ano/ne:\n")
 
 
 """
--- VÝUKA/Pokročilí/opakovani/README.md
+++ VÝUKA/Pokročilí/opakovani/README.md
@@ -8,7 +8,7 @@
 - typy proměnných (int, str, float, bool) a operace s nimi
   - jestli se dá sčítat string s číslem
   - jestli se dá násobit text
-  - a tak dále - je důležíté mít povědomí o tom jak a proč se to chová zrovna takto
+  - a tak dále - je důležité mít povědomí o tom jak a proč se to chová zrovna takto
 - funkce na **přetypování**
   - `str()`, `int()`
 - mít povědomí o funkcích (vlastnostech) stringu jako jsou `startswith()`, `count()` a dalších metodách [stringu](https://www.w3schools.com/python/python_ref_string.asp).
