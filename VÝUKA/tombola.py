7cfc106924992a7bb6707a1a5e5c72536a338942
Michal Janecek <michal.janecek@gmail.com>
2023-09-15 19:58:00 +0200

    tombola: 1 hodina

--- VÝUKA/tombola.py
+++ VÝUKA/tombola.py
@@ -18,6 +18,29 @@ CENY PRO VÍTĚZE: Krypto
 
 """
 
-osudi_1 = {}
-osudi_2 = {}
-osudi_3 = {}
+# Mladší začátečníci
+osudi_1 = {
+    "Eda": [5],
+    "": [4],  # TODO: Kdo je majty?
+    "Kuba": [3],
+    "Ondra": [2],
+    "Aleš": [1]
+}
+
+# Starší začátečníci
+osudi_2 = {
+    "Richard": [5],
+    "Matěj": [4],
+    "Šimon": [3],
+    "Štěpán": [2],  # TODO: Kontrola (lednacekledna?)
+    "Andreii": [1]
+}
+
+# Pokročilí
+osudi_3 = {
+    "Martin": [5],
+    "Petr": [4],
+    "Honza": [3],
+    "David": [2],
+    "Oskar": [1]
+}
