511ed75d66ad51ee4190e2b7c5f1fd75fcc17049
Michal Janecek <michal.janecek@gmail.com>
2023-11-25 18:51:31 +0100

    tombola listky

--- VÝUKA/tombola.py
+++ VÝUKA/tombola.py
@@ -20,12 +20,12 @@ CENY PRO VÍTĚZE: Krypto
 
 # Mladší začátečníci
 osudi_1 = {
-    "Eda": [5, 1, 4, 4, 2],
-    "Albert": [4, 2, 2, 2, 1],  # majty
-    "Kuba": [3, 3, 1, 4, 3, 4],
+    "Eda": [5, 1, 4, 4, 2, 2],
+    "Albert": [4, 2, 2, 2, 1, 4],  # majty
+    "Kuba": [3, 3, 1, 4, 3, 4, 3],
     "Ondra": [2, 2, 1, 3, 5],
-    "Aleš": [1, 3, 5, 5, 1, 3, 3], # kuře
-    "Vašek": [5, 1],
+    "Aleš": [1, 3, 5, 5, 1, 3, 3, 5], # kuře
+    "Vašek": [5, 1, 1],
     "Andrej": [4, 4], # drak
     "Sonia": [3, 1, 5, 1]
 }
@@ -33,23 +33,23 @@ osudi_1 = {
 
 # Starší začátečníci
 osudi_2 = {
-    "Richard": [5, 5, 3, 3, 3, 4, 2], # paul
+    "Richard": [5, 5, 3, 3, 3, 4, 2, 4], # paul
     "Matěj": [4, 4, 4, 4, 5, 1],
-    "Šimon": [3, 3, 2, 1, 3, 5],
-    "Štěpán": [2, 3, 5, 2, 5], # lednacek
-    "Andreii": [1, 1],
+    "Šimon": [3, 3, 2, 1, 3, 5, 2],
+    "Štěpán": [2, 3, 5, 2, 5, 5], # lednacek
+    "Andreii": [1, 1, 3],
     "Kuba" : [2, 5, 1, 2, 3],
     "Jáchym" : [4, 1, 2, 4, 4], # naruto
-    "Ondra" : [3, 1]
+    "Ondra" : [3, 1, 1]
 }
 
 # Pokročilí
 osudi_3 = {
     "Martin": [5, 5, 1, 3, 5, 4],
-    "Petr": [4, 2, 4, 5, 5],
-    "Honza": [3, 1, 3, 3],
-    "David": [2, 4, 4],
+    "Petr": [4, 2, 4, 5, 5, 1],
+    "Honza": [3, 1, 3, 3, 4],
+    "David": [2, 4, 4, 3],
     "Oskar": [1, 3, 1, 5, 4, 2, 2],
     "Tonda": [2, 3, 1, 1],
-    "Kuba": [2, 2, 1, 3]
+    "Kuba": [2, 2, 1, 3, 2]
 }
