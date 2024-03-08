6b9fce459cc2a52bef4f446e1b539efac8db4255
Michal Janecek <michal.janecek@gmail.com>
2024-03-08 15:41:14 +0100

    rozehrivaci uloha na soubory

---
+++ Témata/soubory/rozehrivaci_uloha.py
@@ -0,0 +1,13 @@
+# Úloha vhodná pro zopakování práce se soubory, na začátku druhé hodiny na toto téma.
+
+"""
+ÚLOHA: Nejdelší jméno
+
+Soubor data/jmena.csv obsahuje řádky se jmény. Každý řádek má dva sloupce JMÉNO, PŘÍJMENÍ (oddělené čárkou).
+Vaším úkolem je udělat program, který zjistí, jaké celé jméno je nejdelší, tedy které má v součtu nejvíce znaků.
+Nestačí říct jméno, musíte udělat program, který bude fungovat, ikdyž se nějaká jména do souboru přidají či odeberou.
+
+BONUS: Nepočítejte do znaků mezery, tedy např. pokud má někdo více příjmení (prostřední jméno), mezery vynechejte
+"""
+
+
