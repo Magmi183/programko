49917e4258f5d50921d7e54e5705508a2e13563e
Michal Janecek <michal.janecek@gmail.com>
2024-04-05 12:51:33 +0200

    ulohy na interaktivni web

--- Témata/web/flask/interaktivni-aplikace/ulohy_1.py
+++ Témata/web/flask/interaktivni-aplikace/ulohy_1.py
@@ -8,7 +8,7 @@
                                     ░╚═════╝░╚══════╝░╚════╝░╚═╝░░╚═╝░░░╚═╝░░░
 
 Úlohy bez Jinja2
-  ---> v úlohách níže nemusíte používat render_template, abyste vrátili HTML
+  ---> v úlohách níže nemusíte používat render_template, abyste vrátili HTML jako odpověď na POST
 
   Stačí, když si HTML "postavíte" sami přímo uvnitř funkce a vrátíte STRING, např.:
 
@@ -17,15 +17,37 @@
 
 """
 
+"""
+Úloha 1: BMI
+
+Udělejte stránku s formulářem, kde uživatel zadá svojí výšku a váhu.
+Po stisknutí tlačítka submit mu stránka vypočítá a ukáže jeho BMI.
+
+Můžete se inspirovat stránkou "kalkulator" v ukázce.
+
+BONUS:
+ a) Dejte uživateli na výběr, jestli chce zadat výšku v METRECH nebo CENTIMETRECH.
+    - použijte např. checkbox, nebo radio button
 
 """
-Napady:
-- anketa, hlasovani, vysledky se zobrazi po odhlasovani
-    - BONUS: Zakazat hlasovat vicekrat ze stejne IP!
-- konvertor jednotek
-- konvertor kurzů (může využívat live API!)
-- sber nápadu - ostatni uzivatele mohou o napadech hlasovat
+
+
+"""
+Úloha 2: Převodník jednotek
+
+Realizujte stránku na převod jednotek.
+Uživatel si bude moci vybrat ze seznamu jednotek ODKUD a KAM chce převádět.
+Např.: mm, cm, m, km
+
 """
 
+"""
+Úloha 3: Kurz Krypta
 
-# TODO: Po dokonceni tohoto tematu pridat do "klient" i sekci na POST, a nasledne vytvori nejake klienty co posilaji data.
+Udělejte stránku, kde uživatel bude moci zadat JAKOU a KOLIK kryptoměny vlastní.
+Stránka mu následně ukáže celkovou hodnotu.
+
+Kurz kryptoměny si buď můžete vymyslet, nebo (těžší ale lepší verze) udělejte API call na stránku, na které se nachází opravdový kurz,
+a ten potom použijte (podobně jako v úlohách na téma klient). 
+
+"""
