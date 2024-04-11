f32d4db64dafc8c216a74b404ee74857c7af63ed
Michal Janecek <michal.janecek@gmail.com>
2024-04-11 16:13:07 +0200

    reindex úloh na funkce

--- Témata/funkce/ulohy.py
+++ Témata/funkce/ulohy.py
@@ -20,8 +20,90 @@ NÁVRATOVÁ HODNOTA:
 """
 
 
+# PŘEVODNÍK JEDNOTEK - ÚLOHA 2.
+# >>>> Následující 3 úlohy na sebe postupně navazují
+
 """
-Úkol 2: Random příklad
+>   Úloha 2a: Převodník jednotek (teplota)
+
+    Udělej funkci, která převede stupně Celsia na stupně Fahrenheita a naopak.
+
+    PARAMETRY FUNKCE:
+        - číslo (stupně k převodu)
+        - mód (který bude udávat, jestli se převádí z C do F, nebo naopak)
+
+    NÁVRATOVÁ HODNOTA:
+        - převedené stupně
+
+"""
+
+"""
+>   Úloha 2b: Převodník jednotek (délka)
+
+    Udělej funkci, která bude převádět mezi palci a centimetry.
+
+    PARAMETRY FUNKCE:
+        - číslo (délka k převodu)
+        - mód (který bude udávat, jestli se z palců do cm, nebo naopak)
+
+    NÁVRATOVÁ HODNOTA:
+        - převedená délka
+
+"""
+
+"""
+>   Úloha 2c: Převodník jednotek (univerzální)
+
+    Udělej funkci, která bude umět převádět jak délku tak stupně, podle toho co si uživatel přeje.
+
+    PARAMETRY FUNKCE:
+        - číslo (hodnota k převodu)
+        - další parametry, dle kterých lze specifikovat, jestli chceme převádět délku či stupně, a také odkud kam
+
+    NÁVRATOVÁ HODNOTA:
+        - převedená hodnota (např. stupně z F do C), podle toho co si uživatel přál
+
+"""
+
+"""
+Úloha 3: BMI Kalkulátor
+
+Na základě tělesné váhy a výšky lze pro každého člověka spočítat hodnota BMI.
+BMI, neboli Body Mass Index je číslo, které nějakým způsobem vyjadřuje, jestli má člověk zdravou váhu.
+(Pozor: Na děti nemusí přesně fungovat.)
+
+Jak se BMI počítá si najděte na internetu, i to je součástí úkolu.
+
+BONUSOVÝ ÚKOL: Přidejte i evaluaci, tedy řekněte uživateli, do jaké kategorie spadá (podváha, normální váha, nadváha...).
+"""
+
+"""
+Úloha 4: Caesarova šifra
+
+Udělej funkci, která umí zašifrovat/dešifrovat text pomocí Caesarovy šifry.
+Caesarova šifra funguje tak, že vezme slovo a každé písmeno v něm posune o nějaký počet písmen (každé o stejný počet).
+Pokud tedy chceme zašifrovat např. slovo "ahoj" s posunem 2, vznikne nám "cjql", jelikož písmeno "a" se posune na "c",
+písmeno "h" na "j" atd.
+
+Úlohu je vhodné rozdělit si na následující podúlohy:
+    1) Udělej funkci, která zašifruje jedno písmeno. V parametru přijme písmeno a posun, vrátí výsledek.
+       Např. posun("a", 2) vrátí "c".
+       Pokud tě nenapadá, jak posunout písmeno, zeptej se. Můžeš zkusit ChatGPT.
+    2) Uprav funkci tak, aby uměla i dešifrovat. Případně na to vytvoř separátní funkci.
+    3) Udělej funkci, která už umí zašifrovat celá slova. Využij v ní funkce vytvořené v předchozích krocích.
+"""
+
+
+
+
+
+
+
+
+# Úlohy s nižší prioritou:
+
+"""
+Úloha A: Random příklad
 
 Udělej funkci, která vygeneruje náhodný příklad a zobrazí ho uživateli.
 Součástí funkce je i načtení odpovědi.
@@ -40,7 +122,7 @@ def zadej_priklad():
     """Sem pište váš kód"""
 
 """
-Úkol 3: Počítadlo číslic
+Úloha B: Počítadlo číslic
 
 Udělejte funkci, která v parametru přijímá string. Funkce spočítá, kolik je v tom stringu číslic a výsledek vrátí.
 
@@ -71,7 +153,7 @@ assert pocitadlo_cislic(slovo) == sum(c.isdigit() for c in slovo)
 
 
 """
-Úloha 4: Je to palindrom?
+Úloha C: Je to palindrom?
 
 Udělej funkci, která uživateli řekne, zda-li je danný string palindrom. Palindrom je text, který se dá číst i pozpátku a má stejný význam.
 
@@ -80,80 +162,4 @@ PARAMETRY FUNKCE:
 
 NÁVRATOVÉ HODNOTA:
     - boolean (True nebo False), podle toho, jestli se jedná o palindrom (True), nebo ne (False)
-"""
-
-
-
-
-# PŘEVODNÍK JEDNOTEK - ÚLOHA 5.
-# >>>> Následující 3 úlohy na sebe postupně navazují
-
-"""
->   Úloha 5a: Převodník jednotek (teplota)
-
-    Udělej funkci, která převede stupně Celsia na stupně Fahrenheita a naopak.
-
-    PARAMETRY FUNKCE:
-        - číslo (stupně k převodu)
-        - mód (který bude udávat, jestli se převádí z C do F, nebo naopak)
-
-    NÁVRATOVÁ HODNOTA:
-        - převedené stupně
-
-"""
-
-"""
->   Úloha 5b: Převodník jednotek (délka)
-
-    Udělej funkci, která bude převádět mezi palci a centimetry.
-
-    PARAMETRY FUNKCE:
-        - číslo (délka k převodu)
-        - mód (který bude udávat, jestli se z palců do cm, nebo naopak)
-
-    NÁVRATOVÁ HODNOTA:
-        - převedená délka
-
-"""
-
-"""
->   Úloha 5c: Převodník jednotek (univerzální)
-
-    Udělej funkci, která bude umět převádět jak délku tak stupně, podle toho co si uživatel přeje.
-
-    PARAMETRY FUNKCE:
-        - číslo (hodnota k převodu)
-        - další parametry, dle kterých lze specifikovat, jestli chceme převádět délku či stupně, a také odkud kam
-
-    NÁVRATOVÁ HODNOTA:
-        - převedená hodnota (např. stupně z F do C), podle toho co si uživatel přál
-
-"""
-
-"""
-    Úloha 6: BMI Kalkulátor
-
-    Na základě tělesné váhy a výšky lze pro každého člověka spočítat hodnota BMI.
-    BMI, neboli Body Mass Index je číslo, které nějakým způsobem vyjadřuje, jestli má člověk zdravou váhu.
-
-    Nejdříve udělejte pouze základní verzi pro dospělé, tedy pouze na základě výšky a váhy.
-
-    DOBROVOLNÝ ÚKOL: Přidejte i evaluaci, tedy řekněte uživateli, do jaké kategorie spadá (podváha, normální váha, nadváha...).
-
-"""
-
-"""
-    Úloha 7: Caesarova šifra
-
-    Udělej funkci, která umí zašifrovat/dešifrovat text pomocí Caesarovy šifry.
-    Caesarova šifra funguje tak, že vezme slovo a každé písmeno v něm posune o nějaký počet písmen (každé o stejný počet).
-    Pokud tedy chceme zašifrovat např. slovo "ahoj" s posunem 2, vznikne nám "cjql", jelikož písmeno "a" se posune na "c",
-    písmeno "h" na "j" atd.
-
-    Úlohu je vhodné rozdělit si na následující podúlohy:
-        1) Udělej funkci, která zašifruje jedno písmeno. V parametru přijme písmeno a posun, vrátí výsledek.
-           Např. posun("a", 2) vrátí "c".
-           Pokud tě nenapadá, jak posunout písmeno, zeptej se. Můžeš zkusit ChatGPT.
-        2) Uprav funkci tak, aby uměla i dešifrovat. Případně na to vytvoř separátní funkci.
-        3) Udělej funkci, která už umí zašifrovat celá slova. Využij v ní funkce vytvořené v předchozích krocích.
 """
